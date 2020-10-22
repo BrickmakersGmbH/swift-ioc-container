f181510690511a7b9b63dee24d5b07e4e17109f3
Frank Schaust <frank.schaust@brickmakers.de>
2020-10-22 12:11:23 +0200

    Remove test dependencies from Package.swift

--- Package.swift
+++ Package.swift
@@ -11,13 +11,7 @@ let package = Package(
             name: "Swift_IoC_Container",
             targets: ["Swift_IoC_Container"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
