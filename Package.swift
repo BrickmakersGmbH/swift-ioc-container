6e667f13ceeb62dc127b6d22ff6a81ecd0f9da1c
Frank Schaust <frank.schaust@brickmakers.de>
2020-11-06 13:55:46 +0100

    Add possibility to use as dynamic library

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Swift_IoC_Container",
+            type: .dynamic,
             targets: ["Swift_IoC_Container"]),
     ],
     dependencies: [
